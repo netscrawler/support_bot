ebca89327e4cc371cdfce3226bf7dc6393e03ab9
Daniil Epifanov <mariarthyjamesoo@gmail.com>
2026-01-24 17:28:34 +0300

    feat: start modes and config generation
    feat: start modes and gonfig generation

--- internal/delivery/smb/config.go
+++ internal/delivery/smb/config.go
@@ -1,10 +1,10 @@
 package smb
 
 type SMBConfig struct {
-	Address  string `env:"SMB_ADDRESS"  yaml:"address"`
-	User     string `env:"SMB_USER"     yaml:"user"`
-	Password string `env:"SMB_PASSWORD" yaml:"password"`
-	Domain   string `env:"SMB_DOMAIN"   yaml:"domain"`
-	Share    string `env:"SMB_SHARE"    yaml:"share"`
-	Active   bool   `env:"SMB_ACTIVE"   yaml:"active"`
+	Address  string `env:"SMB_ADDRESS"  yaml:"address"  comment:"Адрес SMB-сервера в формате: host:port\nПример:192.168.1.10:445"`
+	User     string `env:"SMB_USER"     yaml:"user"     comment:"Имя пользователя для аутентификации на SMB-сервере."`
+	Password string `env:"SMB_PASSWORD" yaml:"password" comment:"Пароль пользователя для подключения к SMB-серверу."`
+	Domain   string `env:"SMB_DOMAIN"   yaml:"domain"   comment:"Домен Windows / рабочая группа.\nОбычно: WORKGROUP.\nМожет быть пустым, если домен не используется."`
+	Share    string `env:"SMB_SHARE"    yaml:"share"    comment:"Share — имя сетевой SMB-шары"`
+	Active   bool   `env:"SMB_ACTIVE"   yaml:"active"   comment:"Active — включает использование SMB.\nЕсли false, подключение к SMB не выполняется"`
 }
