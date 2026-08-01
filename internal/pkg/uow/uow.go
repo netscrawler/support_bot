7d8a7087b5c0225cd7ffd8782faaf61888be4d89
Daniil Epifanov <mariarthyjamesoo@gmail.com>
2026-08-02 00:19:25 +0300

    feat(report): enhance report structure with new fields and validation logic

--- internal/pkg/uow/uow.go
+++ internal/pkg/uow/uow.go
@@ -11,6 +11,7 @@ type UOW interface {
 	Commit() error
 	Rollback() error
 	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	GetContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error
 }
 
 type dBUow struct {
