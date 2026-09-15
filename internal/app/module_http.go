5161977b5a976018d9bd6d8e07cf52f4f7f0ecbd
Daniil Epifanov <d.islamov@sendy.land>
2026-09-15 14:24:30 +0300

    feat(app): add httpModule fx.Module

---
+++ internal/app/module_http.go
@@ -0,0 +1,32 @@
+package app
+
+import (
+	"context"
+	"log/slog"
+	apihandlers "support_bot/internal/api/http/handlers"
+	apihttp "support_bot/internal/api/http"
+	"support_bot/internal/config"
+	reportsvc "support_bot/internal/service"
+
+	"go.uber.org/fx"
+)
+
+var httpModule = fx.Module("http", fx.Provide(newReportHandler, newHTTPServer))
+
+func newReportHandler(reportGenSvc *reportsvc.Report, log *slog.Logger) *apihandlers.Handler {
+	return apihandlers.NewHandler(reportGenSvc, log)
+}
+
+func newHTTPServer(cfg *config.Config, reportHandler *apihandlers.Handler, log *slog.Logger, lc fx.Lifecycle) *apihttp.Server {
+	httpSrv := apihttp.New(&cfg.HTTP, reportHandler, log)
+
+	lc.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			httpSrv.Start()
+			return nil
+		},
+		OnStop: httpSrv.Shutdown,
+	})
+
+	return httpSrv
+}
